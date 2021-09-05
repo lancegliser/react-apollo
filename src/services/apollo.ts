266182fe6d1ff1e67d15a8b1e07a177138539b9d
Lance Gliser <lance.gliser@gmail.com>
2021-09-05 15:41:30 -0400

    Define a merge operation for Apollo's InMemoryCache to handle users name space. (#6)
    Co-authored-by: Lance Gliser <lance.gliser@torch.ai>

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -17,7 +17,18 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 });
 
 // Cache implementation
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+  // Define merge strategies for our types and fields
+  typePolicies: {
+    // Top level Query fields
+    Query: {
+      fields: {
+        // Merge our namespaces
+        users: { merge: true },
+      },
+    },
+  },
+});
 
 // Create the apollo client
 export const apolloClient = new ApolloClient({
