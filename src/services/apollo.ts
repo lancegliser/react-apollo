ecf6568f93b6cc787c6d80dbb6f267b7c54fd8c9
Lance Gliser <lance.gliser@torch.ai>
2021-09-05 16:10:16 -0400

    Added services/apollo code to document the link functionality required for authorization and file uploads.

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -2,19 +2,45 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloLink,
-  concat,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const url =
+  process.env.REACT_APP_GRAPHQL_URL || "http://localhost:5000/graphql";
 
 // HTTP connection to the API
-const httpLink = createHttpLink({
+const terminatingLink = createHttpLink({
   // You should use an absolute URL here
-  uri: "http://localhost:5000/graphql",
+  uri: url,
 });
+// An alternative link if you need to support uploads:
+// import { createUploadLink } from "apollo-upload-client";
+// const terminatingLink = createUploadLink({
+//   uri: url,
+// });
 
+// Authorization functionality
+const withToken = setContext(async () => {
+  // const authorization = await authService.getAuthorization();
+  const authorization = "bearer 1234";
+  return { authorization };
+});
 const authMiddleware = new ApolloLink((operation, forward) => {
+  const { authorization } = operation.getContext();
+  operation.setContext(() => ({
+    headers: {
+      Authorization: authorization,
+    },
+  }));
   return forward(operation);
 });
+
+// Apollo link creation layered with middleware to support auth headers and your http link
+const link = ApolloLink.from([
+  withToken,
+  authMiddleware.concat(terminatingLink),
+]);
 
 // Cache implementation
 const cache = new InMemoryCache({
@@ -41,7 +67,4 @@ const cache = new InMemoryCache({
 });
 
 // Create the apollo client
-export const apolloClient = new ApolloClient({
-  link: concat(authMiddleware, httpLink),
-  cache,
-});
+export const apolloClient = new ApolloClient({ link, cache });
