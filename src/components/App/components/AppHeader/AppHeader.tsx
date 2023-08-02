c827491e846f89e92033c1697febb3c95b741428
Lance Gliser <lance.gliser@torch.ai>
2023-08-01 21:25:26 -0500

    Simplified the app header to just a home link.

--- src/components/App/components/AppHeader/AppHeader.tsx
+++ src/components/App/components/AppHeader/AppHeader.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppRouteHome, AppRouteProfile } from "../../../../AppRoutes";
 import "./AppHeader.css";
+import { generateHomePath, routeHomeTitle } from "../../../Home/Router";
 
 const AppHeader: React.FunctionComponent = () => {
   return (
     <header className="AppHeader">
       <nav className="AppHeader-nav">
-        <Link className="AppHeader-link" to={AppRouteHome.path}>
-          {AppRouteHome.title}
-        </Link>
-        <Link className="AppHeader-link" to={AppRouteProfile.path}>
-          {AppRouteProfile.title}
+        <Link className="AppHeader-link" to={generateHomePath()}>
+          {routeHomeTitle}
         </Link>
       </nav>
     </header>
